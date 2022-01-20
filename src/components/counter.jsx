7d311bb787d7b29f55a630a64e3bc1f79bf30af5
Chandan Kumar <chandanofficial.cn@gmail.com>
2022-01-20 14:17:37 +0530

    added delete button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,34 +2,35 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count : 0,
+        value : this.props.counter.value ,
         items :["Mobile", "Watch", "Laptop", "Bag"]
       } 
 
     handleIncremet = () =>{
-        this.setState({count: this.state.count + 1})
+        // this.setState({count: this.state.count + 1})
+        this.setState({value : this.state.value + 1})
     }
 
     
     render() { 
-        const { count } = this.state
+        const { value } = this.state
 
         return (
             <div>
                 <button type="button" className="btn btn-primary m-3 position-relative ">
                     Cart
                     <span className={this.getBadgeClasses()}>
-                        {count === 0 ? '' : count}
+                        {value === 0 ? '' : value}
                         <span className="visually-hidden">unread messages</span>
                     </span>
                 </button>
 
                 <span className={this.getBadgeClasses()}>  </span>
-                <button onClick={this.handleIncremet} className='btn btn-success btn-sm rounded-pill'> add +</button>
-
-                <ul>
+                <button onClick={this.handleIncremet} className='btn btn-success btn-sm rounded-pill'> ADD + </button>
+                <button onClick={() => this.props.onDel(this.props.counter.id)} className="btn btn-sm btn-danger m-2 ">X</button>
+                {/* <ul>
                     {this.state.items.map(item => <li key={item}>{item} </li>)} 
-                </ul>
+                </ul> */}
             </div>
         );
     }
@@ -37,7 +38,7 @@ class Counter extends Component {
     getBadgeClasses() {
         let show_item_no = "position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
         let classes = "d-none";
-        return this.state.count === 0 ? classes : show_item_no;
+        return this.state.value === 0 ? classes : show_item_no;
     }
 }
  
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,12 +3,27 @@ import Counter from "./counter"
 
 class Counters extends Component {
     state = { 
+        counters :[
+            {id:1, value: 4},
+            {id:2, value: 2},
+            {id:3, value: 0},
+            {id:4, value: 1}
+        ]
      } 
+
+     onDel = (counter_id) => {
+         this.setState({counters: this.state.counters.filter(counter => counter.id !== counter_id)})
+     }
     render() { 
         return (
             <div>
-
-                <Counter/>
+                { this.state.counters.map(counter => (
+                <Counter 
+                key={counter.id}
+                counter = {counter}
+                onDel = {this.onDel}
+                /> 
+                ))}
             </div>
         );
     }
